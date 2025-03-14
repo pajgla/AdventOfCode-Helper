95c138f680d42274a0d83411d3af14f3b64c9c5a
Aleksandar Zdravković <pajgla@users.noreply.github.com>
2025-03-14 04:00:59 +0100

    Implementing SolutionExample class

---
+++ AoCHelper/SolutionExample.cs
@@ -0,0 +1,66 @@
+﻿/* ----------------------------- This is an example solution class --------------------------
+ *
+ * Advent of Code 2024 - Day 11 Solution
+ * 
+ * Problem Description:
+ * This puzzle involves processing a sequence of stone numbers according to specific rules:
+ * - If a stone number is 0, it creates a new stone with number 1
+ * - If a stone number has an even number of digits, it splits into two new stones
+ *   (first half digits and second half digits)
+ * - If a stone number has an odd number of digits, it is multiplied by 2024
+ * 
+ * The solution runs for a specified number of "blinks" and calculates the total number
+ * of stones at the end of the process.
+ */
+
+namespace AoCHelper;
+
+[Solver(2024, 11)]
+public class SolutionExampleSolver : BaseSolver<SolutionExample>
+{
+    //No need to have anything here
+}
+
+public class SolutionExample : BaseSolution
+{
+    //You can define members here
+    private bool m_ExampleBool = false;
+    private Dictionary<int, bool> m_ExampleMap = new();
+    
+    //You can also define config members (in PlutonianPebbles example, we have different amount of blinks in first and second exercise part)
+    private int m_Blinks = 25;
+
+    //Initialize your solution based on the exercise part
+    public override void Initialize(ESolutionPart part)
+    {
+        if (part == ESolutionPart.PartOne)
+        {
+            m_Blinks = 25;
+        }
+        else
+        {
+            m_Blinks = 75;
+        }
+    }
+    
+    //Once your program solves part one, you have to reset your member variables for part 2
+    public override void Reset()
+    {
+        m_ExampleBool = false;
+        m_ExampleMap.Clear();
+    }
+
+    //Write your solution here and return the output as string
+    public override string Solve(string input)
+    {
+        //Process puzzle input
+        string[] inputArray = input.Split(' ');
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            m_ExampleMap[int.Parse(inputArray[i])] = true;
+        }
+
+        DebugPrint("You can also write debug prints if you need them!");
+        return $"{m_ExampleMap[0]}";
+    }
+}
